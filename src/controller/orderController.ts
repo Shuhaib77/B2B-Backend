45e04fd96fdbde9d23edd04bd7122ae139e32a67
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-03-14 16:02:10 +0530

    usermodal changed

--- src/controller/orderController.ts
+++ src/controller/orderController.ts
@@ -4,16 +4,13 @@ import { placeOrderService } from "../service/orderService";
 
 export const orderProduct=async(req:Request,res:Response) :Promise<void>=>{
   const {buyerId,sellerId,products,address}=req.body;
-
   try {
     const result= await placeOrderService(buyerId,sellerId,products,address)
-
     if(!result){
         res.status(400).json({message:'failed to place order '})
         return;
     }
-
-    res.status(201).json({message:'order placed successfully'})
+        res.status(201).json({message:'order placed successfully'})
   } catch (error) {
     res.status(500).json({message:"internal server error"})
   }
